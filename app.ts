1de5eece3766e1fb4a2442a27a35a2dc1695358c
YiroYi <yiroyi@live.com>
2021-10-24 18:23:28 -0500

    Function types & Callbacks

--- app.ts
+++ app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
    console.log('Result' + num);
 }
 
+function addAndHandler(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12))
 
 let combineValues: (a: number, b: number) => number;
@@ -14,3 +19,6 @@ combineValues = add;
 
 console.log(combineValues(8, 8));
 //void: means this function do not return anything
+addAndHandler(10, 20, (result) => {
+  console.log(result)
+});
