3add897fb2670423a12326184c4eeb46a96f7ec9
YiroYi <yiroyi@live.com>
2021-11-06 18:19:45 -0600

    Generic function

--- app.ts
+++ app.ts
@@ -1,7 +1,16 @@
-const names: Array<string> = ['Yiro', 'Yujin']
+// const names: Array<string> = ['Yiro', 'Yujin']
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('This is done!');
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done!');
+//   }, 2000);
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB)
+}
+
+const mergedObj = merge({name: 'Yiro'}, {age: 30});
+
+console.log(mergedObj);
+
